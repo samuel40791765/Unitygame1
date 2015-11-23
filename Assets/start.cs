e63f29eae1e59fa9f5451616540d448b32e01aa6
eric <clhsu123@gmail.com>
2015-11-24 01:14:28 +0800

    Add start scene

---
+++ Assets/start.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int touchCount = Input.touchCount;
+        if (touchCount > 0)
+        {
+            for (int i = 0; i < touchCount; i++)
+            {
+                Touch touch = Input.GetTouch(i);
+                if (this.GetComponent<GUITexture>().HitTest(touch.position))
+                {
+                    if (this.name == "Start")
+                        Application.LoadLevel("Scene1");
+                }
+            }
+        }
+
+    }
+}
+
