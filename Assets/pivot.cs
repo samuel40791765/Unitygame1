fc9abddcfdfc5e876a7defb74f75729a8d48b60a
eric <clhsu123@gmail.com>
2015-12-09 01:02:48 +0800

    edit texture

---
+++ Assets/pivot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pivot : MonoBehaviour {
+    public GameObject drop;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = drop.transform.position;
+	}
+}
