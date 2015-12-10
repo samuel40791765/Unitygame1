68aebc3a00a1ab2a24bdd73054444a5a1dca9677
eric <clhsu123@gmail.com>
2015-12-11 05:19:53 +0800

    血量

---
+++ Assets/drophealth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drophealth : MonoBehaviour {
+    public GameObject water;
+    private float timer=0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer = timer + Time.deltaTime;
+        water.transform.localPosition = water.transform.localPosition-new Vector3(0,timer/20,0);
+        
+    }
+}
