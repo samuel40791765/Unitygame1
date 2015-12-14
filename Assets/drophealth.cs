32ccab02bc6d8308474af38a3d2edccd0747dc7d
tom <samuelchungchiang@gmail.com>
2015-12-14 23:47:33 +0800

    roots in desert and health tweaks

--- Assets/drophealth.cs
+++ Assets/drophealth.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class drophealth : MonoBehaviour {
     public GameObject water;
-    private float timer=0;
+	static public float health=0;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,8 +11,11 @@ public class drophealth : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        timer = timer + Time.deltaTime;
-        water.transform.localPosition = water.transform.localPosition-new Vector3(0,timer/20,0);
-        
+        health = health + Time.deltaTime;
+		Debug.Log (+ health);
+        water.transform.localPosition = water.transform.localPosition-new Vector3(0,0.1f,0);
+        if (health >= 32) {
+			Application.LoadLevel("LevelEnd");
+		}
     }
 }
---
+++ Assets/dry.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dry : MonoBehaviour {
+	public GameObject water;
+	public GameObject ball;
+	// Use this for initialization
+	void OnTriggerEnter (Collider other) {
+		Debug.Log("Player OnTriggerEnter");
+		drophealth.health = drophealth.health + 1;
+		water.transform.localPosition = water.transform.localPosition-new Vector3(0,3.3f,0);
+	}
+}
