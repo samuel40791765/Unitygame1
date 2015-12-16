8e6f9cf676b87a46aa966dfe13b857fb62e8e5ca
eric <clhsu123@gmail.com>
2015-12-17 02:03:28 +0800

    Mechanics

---
+++ Assets/cross.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cross : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(0, 1, 0);
+	}
+}
