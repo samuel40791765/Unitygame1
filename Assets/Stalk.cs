5b1cdca45648e6dd0b0523093a3446127963f240
tom <samuelchungchiang@gmail.com>
2015-12-22 23:56:41 +0800

    added spider animations

--- Assets/Stalk.cs
+++ Assets/Stalk.cs
@@ -3,23 +3,41 @@ using System.Collections;
 
 public class Stalk : MonoBehaviour {
     public GameObject prey;
+	public Transform target;
     private Vector3 dir;
     private float dis;
     private float slow;
+	private bool foundprey;
     // Use this for initialization
     void Start () {
-	
+		foundprey = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-       
+		this.transform.LookAt(target);
         dis = Vector3.Distance(this.transform.position, prey.transform.position);
         if (dis <= 60)
         {
-            slow = dis*10;
+            slow = dis*5;
             dir = prey.transform.position - this.transform.position;
+			if (foundprey==false) {
+				this.GetComponent<Animation>().Play ("walk");
+			}
             this.transform.Translate(dir / slow);
         }
+	}
+
+	void OnCollisionEnter (Collision other) {
+		foundprey=true;
+		StartCoroutine (waitsec());
+	}
+
+
+
+	IEnumerator waitsec() {
+		this.GetComponent<Animation>().Play ("attack2",PlayMode.StopAll);
+		yield return new WaitForSeconds(2);
+		foundprey=false;
 	}
 }
