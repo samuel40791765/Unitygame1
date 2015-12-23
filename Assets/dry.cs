b40478a8a2e9982e3d2046f08695fd6dd2576e39
tom <samuelchungchiang@gmail.com>
2015-12-23 14:26:04 +0800

    fire and oil package plus root fix

--- Assets/dry.cs
+++ Assets/dry.cs
@@ -5,10 +5,18 @@ public class dry : MonoBehaviour {
 	public GameObject water;
 	// Use this for initialization
 	void OnCollisionEnter (Collision other) {
-        if (other.gameObject.name == "drop")
-        {
-            Debug.Log("Player OnCollisionEnter");
-            water.transform.position = water.transform.position - new Vector3(0, 15f, 0);
-        }
+		if (other.gameObject.name == "drop")
+		{
+			Debug.Log("Player OnCollisionEnter");
+			water.transform.position = water.transform.position - new Vector3(0, 15f, 0);
+		}
+	}
+
+	void OnTriggerEnter (Collision other) {
+		if (other.gameObject.name == "drop")
+		{
+			Debug.Log("Player OnCollisionEnter");
+			water.transform.position = water.transform.position - new Vector3(0, 15f, 0);
+		}
 	}
 }
