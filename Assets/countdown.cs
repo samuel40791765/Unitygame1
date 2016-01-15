4b9180cb99073ff929e4cebbe3b60d31759c0b16
B10332024 <samuel19960920@yahoo.com>
2016-01-15 13:53:00 +0800

    countdown go!

---
+++ Assets/countdown.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class countdown : MonoBehaviour {
+	private float starttime;
+	private int timeleft;
+	public GUIStyle style;
+	private bool show;
+	// Use this for initialization
+	void Start () {
+		starttime = 6;
+		show = true;
+		destination.AtEnd = true;
+		StartCoroutine (hide ());
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		if (show) {
+			if(timeleft!=0){
+				GUI.Label (new Rect (650, 650, 300, 50), (timeleft.ToString ()), style);
+			}
+			else{
+				GUI.Label (new Rect (650, 650, 300, 50), ("GO!"), style);
+			}
+			starttime = starttime - Time.deltaTime;
+			timeleft=(int)starttime;
+		}
+	}
+
+
+
+	IEnumerator hide() {
+		yield return new WaitForSeconds (3);
+		show = false;
+		destination.AtEnd = false;
+	}
+}
